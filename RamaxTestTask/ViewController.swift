9e05225849267e6cce4a3aeca2b1da0b99ab6456
Vladimir Mazunin <mazunin-v.k@yandex.ru>
2019-08-18 03:51:32 +0300

    Initial Commit

---
+++ RamaxTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RamaxTestTask
+//
+//  Created by Vladimir Mazunin on 18/08/2019.
+//  Copyright © 2019 Vladimir Mazunin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
